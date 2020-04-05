ee7d538313dc0c8f870daa6396673a643e98ae90
Doro402 <63165530+Doro402@users.noreply.github.com>
2020-04-05 22:02:34 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,76 @@
- <?php
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXEECICE 3</title>
+<link rel="stylesheet"href="css/exo1.css">
+</head>
+<body>
+<center>
+<form method="POST" action="" >
+    
+            <h4>Saisir des mots</h4> 
+            <textarea name="mot"  cols="30" rows="10"></textarea> <br><br>  
+            <input type="submit" name="numero" value="valider">
+        
+        </form>
+</center>
+    </body>
+</html>
+<?php
+
+
+
+
+function nombrdecaractere ($n) {
+if (strlen($n) <=20){
+$bool=true ;
+
+}
+else {
+$bool=false;}
+
+return $bool;
+}
+function tableau ($t , array $tab) {
+    $letter="/m/i";
+    $k=0;
+    $tableau=explode(" ",$t);
+    $a= count($tableau);
+    for ($i=0; $i<$a; $i++){
+        if (nombrdecaractere(@$tableau[$i])){
+            if (!filter_var(@$tableau[$i],FILTER_SANITIZE_NUMBER_INT)){
+                $tab=@$tableau[$i];
+                echo $tab. ' ';
+            }
+            if (preg_match($letter,@$tableau[$i])){
+                $k=$k+1;
+            }
+        }else{
+            echo 'Le nombre de caractere saisi doit etre inferieur ou egal a 20';
+        }
+
+
+    }
+    echo 'le nombre de mot contenant la lettre M'. $k;
+    return $tab;
+}
+$tabs=0;
+if (empty($_POST['mot'])){
+    echo 'remplir le champ';
+}
+else {
+     tableau($_POST['mot'], array($tabs));
+
+
+}
+if (preg_match("/^[A-Z]/",$r)){
+    echo"maj";
+}
+
+?>
 
 $message='';
 
@@ -7,23 +78,3 @@ $message='';
 ?>
  
  
- <!DOCTYPE html>
-        <head>
-        <title></title>
-        </head>
- <body>
-     <form method="POST">
-         <label for="m"entrer le nombre de champs></label>
-     <input type="text" name="nbre" placeholder="entrer NOMBRE DE CHAMPS">
-     <p style='color:blue;'><?=$message?></p>
-     <input type="submit" value= "valider" name="valider"> <br><br>
-     <?php for ($i=0;$i<$nombreChamps;$i++){?>
-        <laber for= "m"> Mot numero<?=$i+1?></label>
-        <input type="text" name="mots_<?= $i ?>"autocomplete="off">
-        <?php }?>
-        <?php if($nombreChamps>0) { ?>
-            <button type="submit" name="resultat"> Resultats </button>
-            <?php }?>
-     </form>
- </body>
-
